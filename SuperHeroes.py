60ccbfdccc744aac061e8cd65bb41764061ff554
elgrian <anwar.loves.lacroix@gmail.com>
2018-09-21 10:01:04 -0700

    first commit

--- SuperHeroes.py
+++ SuperHeroes.py
@@ -15,7 +15,7 @@ class Ability:
 
 
 
-# ----------------------------------------------------------------------------
+# -------------------------------** Hero Class **---------------------------------------------
 
 
 class Hero:
@@ -30,8 +30,8 @@ class Hero:
         self.deaths = 0
         self.kills = 0
 
-    """ This method should run the defend method on each piece of armor and calculate the total defense.
-    If the hero's health is 0, the hero is out of play and should return 0 defense points.
+    """ 1. This method should run the defend method on each piece of armor and calculate the total defense.
+    2. If the hero's health is 0, the hero is out of play and should return 0 defense points.
     """
     def defend(self):
         if (self.health > 0): ##Come back to this and see if a while loops will work
@@ -40,9 +40,9 @@ class Hero:
         return defence
 
     """
-    This method should subtract the damage amount from the
+    1. This method should subtract the damage amount from the
     hero's health.
-    If the hero dies update number of deaths.
+    2. If the hero dies update number of deaths.
     """
     def take_damage(self, damage_amt):
         self.health -= damage_amt
@@ -52,7 +52,7 @@ class Hero:
         return 0
 
 
-    """ This method should add the number of kills to self.kills"""
+    """ 1. This method should add the number of kills to self.kills"""
     def add_kill(self, num_kills):
         self.kills += num_kills
 
@@ -81,13 +81,16 @@ class Team:
         """Instantiate Resources"""
         self.name = team_name
         self.heroes = list()
+        self.team_kills = 0
 
+
+    """ 1. Add Hero object to the list"""
     def add_hero(self, Hero):
-        """Add Hero object to the list"""
-        self.heroes.append(Hero) # Think this is right
+        self.heroes.append(Hero)
 
+
+    """ 1. Remove hero from hero list. If hero isn't found, return 0"""
     def remove_hero(self, name):
-        """Remove hero from hero list. If hero isn't found, return 0"""
         if self.name != None:
             for hero in self.view_all_heroes:
                 if name in hero.name:
@@ -97,8 +100,8 @@ class Team:
         else:
             return 0
 
+    """ 1. Find and return hero from list. If hero isn't found, return 0"""
     def find_hero(self, name):
-        """Find and return hero from list. If hero isn't found, return 0"""
         if self.heroes != None:
             if name == hero.name:
                 return heroe
@@ -106,11 +109,48 @@ class Team:
                 return 0
 
     def view_all_heroes(self):
-        """Print out all heroes to the console"""
+        """ 1. Print out all heroes to the console"""
         for hero in self.heroes:
             name = hero.name
             print(name)
 
+
+    """ 1. This method should total our teams attack strength and call the defend() method
+    on the rival team that is passed in.
+    2. It should call add_kill() on each hero with the number of kills made."""
+    def attack(self, other_team):
+
+
+    """ 1. This method should calculate our team's total defense.
+    2. Any damage in excess of our team's total defense should be evenly distributed amongst
+    all heroes with the deal_damage() method.
+    3. Return number of heroes killed in attack."""
+    def defend(self, damage_amt):
+
+
+
+    """ 1. Divide the total damage amongst all heroes.
+    2. Return the number of heroes that died in attack."""
+    def deal_damage(self, damage):
+    
+
+    """ 1. this method should reset all heroes health  to their original starting value. """
+    def revive_heroes(self, health = 100):
+
+
+
+    """ 1. This methof should print the ratio of kills/deaths for each member of the team to the screen.
+    2. This data must be output to the terminal. """
+    def stats(self):
+
+
+
+    """ 1. This method should update each hero when there is a team kill. """
+    def update_kills(self):
+
+
+
+#-------------------------------** Armor Class ** -----------------------------
 class Armor:
     def __init__(self, name, defense):
         """Instantiate name and defense strength."""
